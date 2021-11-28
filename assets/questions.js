6f590dbeba31713ddb48d63d82054f7329c1b3ed
kasai <kasaipreston@gmail.com>
2021-11-27 18:03:04 -0800

    working on displaying questions

---
+++ assets/questions.js
@@ -0,0 +1,33 @@
+console.log("hi!");
+
+var questions = [
+    {
+        text: "two plus two equals?",
+        choice: [
+            "one",
+            "two",
+            "four"
+        ],
+        correct: "four"
+    },
+
+    {
+        text: "one plus one equals?",
+        choice: [
+            "one",
+            "two",
+            "four"
+        ],
+        correct: "two"
+    },
+
+    {
+        text: "one times zero equals?",
+        choice: [
+            "one",
+            "two",
+            "four"
+        ],
+        correct: "one"
+    },
+]
--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,83 @@
-console.log("hi!");
 
-var questions = [
-    {
-        question: "two plus two equals?",
-        multipleChoice: [
-            "one",
-            "two",
-            "four"
-        ],
-        correct: "four"
-    },
-
-    {
-        question: "one plus one equals?",
-        multipleChoice: [
-            "one",
-            "two",
-            "four"
-        ],
-        correct: "two"
-    },
-
-    {
-        question: "one times zero equals?",
-        multipleChoice: [
-            "one",
-            "two",
-            "four"
-        ],
-        correct: "one"
-    },
-]
+// variables
 
 var startButton = document.getElementById("start-btn");
-var welcome = document.querySelector(".start-screen");
 
-startButton.addEventListener("click", function() {
-        welcome.setAttribute("style", "display:none;");
-        displayQuestions(questions)
+var hideStartScreen = document.querySelector(".starting-screen");
 
-});
+var timer = document.getElementById("time");
 
-function displayQuestions()
+// var multipleChoice = document.querySelector("choice-buttons");
+
+var secondsLeft = 30;
+var timerInterval;
+
+startButton.addEventListener("click", function () {
+    hideStartScreen.setAttribute("style", "display:none;")
+
+   
+    countdown();
+    displayQuestion();
+}); 
+
+
+var currentIndex = 0;
+var currentQuestion;
+
+var questionText = document.getElementById("question-text");
+
+var choiceButtons = document.getElementById("choice-buttons");
+
+var questionText = document.getElementById("questionContent");
+
+function displayQuestion() {
+    for (var i = 0; i < questions.length; i++) {
+        if (currentIndex >= questions.length) {
+            endQuiz();
+
+        console.log("questions.length" + questions.length);
+        }
+        
+    }
+
+
+
+function checkAnswer(event) {
+    console.log(currentQuestion);
+    console.log(event.target.value);
+
+    if (event.target.value === currentQuestion.correct) {
+        console.log("Correct!");
+
+    }
+    else {
+        console.log("nope");
+        timeLeft = timeLeft - 3;
+    }
+    currentIndex++
+    displayQuestion();
+}
+
+function nexrQuestion() {
+
+}
+
+// timer
+function countdown() {
+    timerInterval = setInterval(function () {
+        timer.textContent = "Time remaining: " + secondsLeft;
+        secondsLeft--;
+
+        if (secondsLeft <= -1) {
+            endQuiz();
+        }
+    }, 1000);
+}
+
+function endQuiz() {
+    // Clear timer
+    clearInterval(timerInterval);
+
+    questionText.setAttribute("style", "display:none;");
+
+}
