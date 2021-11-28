193f1e8e249bcbbaed1be23ef3ab62f10828b553
kasai <kasaipreston@gmail.com>
2021-11-28 09:06:30 -0800

    gettting questions to cycle

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,37 @@
 
-// variables
-
+// Variables for start button, hiding start screen, and modifying the timer.
 var startButton = document.getElementById("start-btn");
 
 var hideStartScreen = document.querySelector(".starting-screen");
 
 var timer = document.getElementById("time");
 
-// var multipleChoice = document.querySelector("choice-buttons");
-
 var secondsLeft = 30;
+
 var timerInterval;
+
+// Variable for my index pointer, and empty var's to store data in.
+var currentIndex = 0;
+
+var currentQuestion;
+
+var choiceButton;
 
 startButton.addEventListener("click", function () {
     hideStartScreen.setAttribute("style", "display:none;")
 
-   
-    countdown();
+
+    countDown();
     displayQuestion();
-}); 
+});
 
-
-var currentIndex = 0;
-var currentQuestion;
-
-var questionText = document.getElementById("question-text");
-
-var choiceButtons = document.getElementById("choice-buttons");
+// Variables to store functions to print question text and buttons to html.
+var buttonSelection= document.getElementById("choice-buttons");
 
 var questionText = document.getElementById("questionContent");
 
-function displayQuestion() {
-    for (var i = 0; i < questions.length; i++) {
-        if (currentIndex >= questions.length) {
-            endQuiz();
-
-        console.log("questions.length" + questions.length);
-        }
-        
-    }
-
-
-
-function checkAnswer(event) {
-    console.log(currentQuestion);
-    console.log(event.target.value);
-
-    if (event.target.value === currentQuestion.correct) {
-        console.log("Correct!");
-
-    }
-    else {
-        console.log("nope");
-        timeLeft = timeLeft - 3;
-    }
-    currentIndex++
-    displayQuestion();
-}
-
-function nexrQuestion() {
-
-}
-
-// timer
-function countdown() {
+// Timer
+function countDown() {
     timerInterval = setInterval(function () {
         timer.textContent = "Time remaining: " + secondsLeft;
         secondsLeft--;
@@ -74,6 +42,51 @@ function countdown() {
     }, 1000);
 }
 
+function displayQuestion() {
+
+    var questionTitle = document.getElementById("question-title");
+
+    for (var i = 0; i < questions.length; i++) {
+        if (currentIndex >= questions.length) {
+
+            endQuiz();
+        }
+    }
+
+
+    questionTitle.textContent = questions[currentIndex].text;
+
+    for (var i = 0; i < questions[currentIndex].choice.length; i++) {
+        buttonSelection = document.createElement("button");
+
+        choiceButton.textContent = questions[currentIndex].choice[i];
+        choiceButtons.append(choiceButton);
+    }
+
+    choiceButton.addEventListener("click", function (event) {
+        if (event.target.textContent === questions[currentIndex].correct) {
+            console.log("Correct!");
+        }
+        else {
+            secondsLeft -= 5;
+            console.log("Incorrect");
+        }
+        displayNextQuestion();
+    });
+
+    function displayNextQuestion() {
+        questionTitle.innerHTML = "";
+        choiceButtons.innerHTML = "";
+
+        currentIndex += 1;
+        displayQuestion();
+    }
+
+
+}
+
+
+
 function endQuiz() {
     // Clear timer
     clearInterval(timerInterval);
@@ -81,3 +94,5 @@ function endQuiz() {
     questionText.setAttribute("style", "display:none;");
 
 }
+
+
